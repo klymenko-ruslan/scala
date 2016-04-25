caaac3a9e08c8feb476a080562e1d3f1609be774
boosterit <booster.it@yandex.ru>
2016-04-25 16:22:52 +0300

    --Reverted

--- src/scalaSnake/Client.scala
+++
@@ -1,8 +0,0 @@
-package scalaSnake
-
-/**
- * Created by klymenko.ruslan on 25.04.2015.
- */
-object Client {
-  def main(args : Array[String]) = new TheSnakeGame(SnakeGameField.DEFAULT_FIELD_SIZE, Speed.MIDDLE)
-}
