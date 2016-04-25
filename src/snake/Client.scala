0f1dbb486d4e4dc1287794cb4050905f3324b41b
boosterit <booster.it@yandex.ru>
2016-04-25 16:20:07 +0300

    -- Initial commit.

---
+++ src/snake/Client.scala
@@ -0,0 +1,8 @@
+package snake
+
+/**
+ * Created by klymenko.ruslan on 25.04.2015.
+ */
+object Client {
+  def main(args : Array[String]) = new TheSnakeGame(SnakeGameField.DEFAULT_FIELD_SIZE, Speed.MIDDLE)
+}
